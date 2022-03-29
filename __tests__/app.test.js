df49d10bab8d10a2fb2d7e35b2664a832e6dac15
Akin Fagbohun <a_fagbohun@yahoo.co.uk>
2022-03-29 08:25:29 +0100

    modified tests to make more general

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,4 @@
-const app = require('../index');
+const app = require('../app');
 const request = require('supertest');
 const db = require('../db/connection');
 const express = require('express');
@@ -8,41 +8,49 @@ const testData = require('../db/data/test-data/index')
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
-describe("GET /api/topics", () => {
+describe('GET /api/topics', () => {
+  test('sends GET to endpoint -> responds with array', () => {
+    return request(app)
+      .get('/api/topics')
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toBeInstanceOf(Array);
+      });
+  });
   test('sends GET to endpoint -> responds with object with two properties', () => {
     return request(app)
       .get('/api/topics')
       .expect(200)
       .then((res) => {
-        expect(res.body[0]).toMatchObject({
-          description: 'The man, the Mitch, the legend',
-          slug: 'mitch'
+        const body = res.body;
+        body.forEach(topic => {
+          expect(topic).toBeInstanceOf(Object);
+          expect(Object.keys(topic).length).toBe(2);
         });
       });
   });
-  test('sends GET to endpoint -> responds with array with two properties', () => {
+  test('sends GET to endpoint -> checks object properties and values', () => {
     return request(app)
       .get('/api/topics')
       .expect(200)
       .then((res) => {
-        const arr = [
-          {
-            description: 'The man, the Mitch, the legend',
-            slug: 'mitch'
-          },
-          {
-            description: 'Not dogs',
-            slug: 'cats'
-          },
-          {
-            description: 'what books are made of',
-            slug: 'paper'
-          }
-        ];
-        expect(res.body).toBeInstanceOf(Array);
-        expect(res.body).toMatchObject(arr);
+        const body = res.body;
+        body.forEach(topic => {
+          expect(topic).toHaveProperty('description');
+          expect(topic).toHaveProperty('slug');
+          expect(typeof Object.values(topic)[0]).toBe('string');
+          expect(typeof Object.values(topic)[1]).toBe('string');
+        });
       });
   });
 });
+describe('GET /api/topisc', () => {
+  test('sends GET to incorrect endpoint -> returns 404', () => {
+    return request(app)
+      .get('/api/topisc')
+      .expect(404)
+  });
+});
+
 
 
