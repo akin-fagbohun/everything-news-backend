7f5ba7985e7a060004eabb9ce3cd6771e35e2528
Akin Fagbohun <a_fagbohun@yahoo.co.uk>
2022-03-31 15:18:05 +0100

    MVC pattern observed

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,26 +1,11 @@
 const { articleData } = require('../db/data/test-data');
 const articles = require('../db/data/test-data/articles');
 const topics = require('../db/data/test-data/topics');
-const { selectTopics, selectArticles, selectArticleById, selectArticleCommentsById, selectUsers, updateArticleById } = require('../models/models');
+const { selectTopics, selectUsers, } = require('../models/models');
 
 exports.getTopics = (req, res, next) => {
   selectTopics().then((topics) => {
     res.status(200).send(topics)
-  });
-};
-
-exports.getArticleById = (req, res, next) => {
-  const { article_id } = req.params;
-  selectArticleById(article_id).then((article) => {
-      res.status(200).send(article.rows[0]);
-  });
-};
-
-exports.getArticleCommentsById = (req, res, next) => {
-  const { article_id } = req.params;
-  selectArticleCommentsById(article_id).then((article) => {
-    console.log(article.rows);
-      res.status(200).send(article.rows);
   });
 };
 
@@ -30,16 +15,5 @@ exports.getUsers = (req, res, next) => {
   });
 };
 
-exports.getArticles = (req, res, next) => {
-  selectArticles().then((articles) => {
-    res.status(200).send(articles)
-  })
-}
 
-exports.patchArticleById = (req, res, next) => {
-  const { inc_votes: votes } = req.body;
-  const { article_id } = req.params;
-  updateArticleById(article_id, votes).then((article) => {
-    res.status(201).send(article.rows[0])
-  });
-}
+
--- models/models.js
+++ models/models.js
@@ -2,42 +2,17 @@ const db = require('../db/connection');
 const format = require('pg-format');
 const articles = require('../db/data/test-data/articles');
 
+// MISC models that are too few for seperate.
+  // break out into separate model when necessary.
+
 exports.selectTopics = () => {
   return db.query('SELECT * FROM topics').then((topics) => {
     return topics.rows;
   });
 };
 
-exports.selectArticleById = (id) => {
-  return db.query(`
-    SELECT COUNT(comment_id) AS comment_count, articles.*
-    FROM articles
-    LEFT JOIN comments
-    ON comments.article_id = articles.article_id
-    WHERE articles.article_id = $1
-    GROUP BY articles.article_id;`, [id])
-}
-
-exports.selectArticleCommentsById = (id) => {
-  return db.query(`
-    SELECT comment_id, author, body, created_at, votes
-    FROM comments
-    WHERE article_id = $1;`, [id])
-}
-
 exports.selectUsers = () => {
   return db.query('SELECT * FROM users').then((users) => {
     return users.rows;
   });
 };
-
-exports.selectArticles = () => {
-  return db.query('SELECT * FROM articles').then((articles) => {
-    return articles.rows;
-  });
-}
-
-exports.updateArticleById = (id, value) => {
-  const query = format('UPDATE %I SET votes =  votes + $2 WHERE article_id = $1 RETURNING *;', 'articles');
-  return db.query(query, [id, value]);
-}
