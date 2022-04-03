473faa87da9d6331952977174d5933b923c18047
Akin Fagbohun <a_fagbohun@yahoo.co.uk>
2022-04-03 12:29:45 +0100

    fixing port

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const { getArticleById, getArticleCommentsById, getArticles, patchArticleById } 
 
 
 const app = express();
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
 
 // middleware
 app.use(express.json());
