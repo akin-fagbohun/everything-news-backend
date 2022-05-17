4f70e26b34b8a30882f093065655996ae6bd08f7
Akin Fagbohun <a_fagbohun@yahoo.co.uk>
2022-05-17 23:53:42 +0100

    resolving bugs

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,18 +1,18 @@
-const format = require("pg-format");
+const format = require('pg-format');
 const {
   convertTimestampToDate,
   createRef,
   formatComments,
-} = require("../helpers/utils");
-const db = require("../connection");
-const { dropTables, createTables } = require("../helpers/manage-tables");
+} = require('../helpers/utils');
+const db = require('../connection');
+const { dropTables, createTables } = require('../helpers/manage-tables');
 
 const seed = async ({ topicData, userData, articleData, commentData }) => {
   await dropTables();
   await createTables();
 
   const insertTopicsQueryStr = format(
-    "INSERT INTO topics (slug, description) VALUES %L RETURNING *;",
+    'INSERT INTO topics (slug, description) VALUES %L RETURNING *;',
     topicData.map(({ slug, description }) => [slug, description])
   );
   const topicsPromise = db
@@ -20,7 +20,7 @@ const seed = async ({ topicData, userData, articleData, commentData }) => {
     .then((result) => result.rows);
 
   const insertUsersQueryStr = format(
-    "INSERT INTO users ( username, name, avatar_url) VALUES %L RETURNING *;",
+    'INSERT INTO users ( username, name, avatar_url) VALUES %L RETURNING *;',
     userData.map(({ username, name, avatar_url }) => [
       username,
       name,
@@ -35,7 +35,7 @@ const seed = async ({ topicData, userData, articleData, commentData }) => {
 
   const formattedArticleData = articleData.map(convertTimestampToDate);
   const insertArticlesQueryStr = format(
-    "INSERT INTO articles (title, topic, author, body, created_at, votes) VALUES %L RETURNING *;",
+    'INSERT INTO articles (title, topic, author, body, created_at, votes) VALUES %L RETURNING *;',
     formattedArticleData.map(
       ({ title, topic, author, body, created_at, votes = 0 }) => [
         title,
@@ -52,11 +52,11 @@ const seed = async ({ topicData, userData, articleData, commentData }) => {
     .query(insertArticlesQueryStr)
     .then((result) => result.rows);
 
-  const articleIdLookup = createRef(articleRows, "title", "article_id");
+  const articleIdLookup = createRef(articleRows, 'title', 'article_id');
   const formattedCommentData = formatComments(commentData, articleIdLookup);
 
   const insertCommentsQueryStr = format(
-    "INSERT INTO comments (body, author, article_id, votes, created_at) VALUES %L RETURNING *;",
+    'INSERT INTO comments (body, author, article_id, votes, created_at) VALUES %L RETURNING *;',
     formattedCommentData.map(
       ({ body, author, article_id, votes = 0, created_at }) => [
         body,
--- models/topics-model.js
+++ models/topics-model.js
@@ -1,51 +1,52 @@
 const db = require('../db/connection');
-const format = require('pg-format');
-const articles = require('../db/data/test-data/articles');
-const fs = require("fs/promises");
+const fs = require('fs/promises');
 
 // MISC models that are too few for seperate.
-  // break out into separate model when necessary.
+// break out into separate model when necessary.
 
 exports.selectApi = async () => {
-  const endpoints = await fs.readFile(`db/endpoints.json`, "utf8");
+  const endpoints = await fs.readFile(`db/endpoints.json`, 'utf8');
   return endpoints;
 };
 
 exports.selectTopics = () => {
-  return db.query('SELECT * FROM topics')
-  .then((topics) => {
+  return db.query('SELECT * FROM topics').then((topics) => {
     if (topics.rows.length === 0) {
-      return Promise.reject({ status: 404, msg: 'URL not found.' })
+      return Promise.reject({ status: 404, msg: 'URL not found.' });
     }
     return topics.rows;
   });
 };
 
 exports.selectUsers = () => {
-  return db.query('SELECT * FROM users')
-  .then((users) => {
+  return db.query('SELECT * FROM users').then((users) => {
     if (users.rows.length === 0) {
-      return Promise.reject({ status: 404, msg: 'User not found.' })
+      return Promise.reject({ status: 404, msg: 'User not found.' });
     }
-      return users.rows;
+    return users.rows;
   });
 };
 
 exports.addComment = (comment, article_id) => {
-const { username, body } = comment;
-const { article_id: id } = article_id;
-return db.query(`
+  const { username, body } = comment;
+  const { article_id: id } = article_id;
+  return db
+    .query(
+      `
   INSERT INTO comments (author, body, article_id, votes, created_at)
   VALUES ($1, $2, $3, 0, NOW())
-  RETURNING *;`, [username, body, id])
-  .then((result) => {
-    return result.rows[0];
-  });
+  RETURNING *;`,
+      [username, body, id]
+    )
+    .then((result) => {
+      return result.rows[0];
+    });
 };
 
 exports.removeComment = (id) => {
-  return db.query('DELETE FROM comments WHERE comment_id = $1;', [id])
-  .then((result) => {
-    return result.rows;
-  });
+  return db
+    .query('DELETE FROM comments WHERE comment_id = $1;', [id])
+    .then((result) => {
+      return result.rows;
+    });
 };
